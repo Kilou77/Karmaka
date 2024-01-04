373a12efba71086f2e0588d62943f7859c903b88
Kal-tv <tuvee.kylian@gmail.com>
2024-01-04 16:47:41 +0100

    Update Master Branch with "change in main ..."-code + commentary HashMap in CardManager Class

--- src/com/lo02/Karmaka/objects/CardManager.java
+++ src/com/lo02/Karmaka/objects/CardManager.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 public class CardManager {
 
     HashMap<Class<? extends Card>, Integer> cardList = new HashMap<>();
+    /*Création de la HashMap contenant l'ensemble des cartes du jeu*/
 
     public void init() {
         initCardList();
