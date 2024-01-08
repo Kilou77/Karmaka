ff495e7c8fe50f0967bb54df32b961f32c08267f
Kal-tv <tuvee.kylian@gmail.com>
2024-01-08 12:34:46 +0100

    Added random strategy + strategy details

--- src/com/lo02/Karmaka/enums/Strategy.java
+++ src/com/lo02/Karmaka/enums/Strategy.java
@@ -1,5 +1,13 @@
 package com.lo02.Karmaka.enums;
 
 public enum Strategy {
-    OFFENSIVE(), DEFENSIVE()
+    OFFENSIVE(), DEFENSIVE(), RANDOM()
+    /* offensive = (60% de jouer une carte rouge, 15% vert, 15% bleu, 10% mosaïque)
+    defensive = (60% vert, 15% bleu, 15% mosaïque, 10% rouge)
+    random = nombre aléatoire entre 1 et 4
+    1 : rouge
+    2 : bleu
+    3 : vert
+    4 : mosaïque
+     */
 }
